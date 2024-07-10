3e958d82c5fdb35cffa317f09b50b2e9ee96c4eb
Caio Teraoka <caioteraoka16@gmail.com>
2024-07-10 19:26:26 -0300

    First projects

---
+++ blink/Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/core_cm3.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/cmsis_version.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/core_cm3.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/cmsis_version.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/cmsis_compiler.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Include/cmsis_gcc.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/blink/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
---
+++ button/Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/core_cm3.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/cmsis_version.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/core_cm3.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/cmsis_version.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/cmsis_compiler.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Include/cmsis_gcc.h:
+C:/Users/caiom/Documents/STM32F1_BareMetal/button/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
